3f8b213fded38d85e703e866aba065310e7713e1
Thomas <26279201+pinouche@users.noreply.github.com>
2022-12-21 13:57:38 +0100

    add abalone dataset

--- datasets/load_data.py
+++ datasets/load_data.py
@@ -1,18 +1,23 @@
+import pandas as pd
 
+from datasets.synthetic_datasets import make_two_spheres, make_alternate_stripes, circles_data
 
 
 def load_data(dataset):
 
     if dataset == "spheres":
-        data_x = make_two_spheres()
+        data = make_two_spheres()
 
     elif dataset == "circles":
-        data_x = circles_data()
+        data = circles_data()
 
     elif dataset == "alternate_stripes":
-        data_x = make_alternate_stripes()
+        data = make_alternate_stripes()
+
+    elif dataset == "abalone":
+        data = pd.read_csv("abalone.data")
 
     else:
         raise ValueError(f"dataset {dataset} is not valid.")
 
-    return data_x
+    return data
