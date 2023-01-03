36b5b9ccafe8b34df6e8a63aa81f3da832339947
Thomas <26279201+pinouche@users.noreply.github.com>
2023-01-03 15:28:02 +0100

    use json config file to input parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
+import json
+import argparse
 import pickle
 import warnings
 warnings.filterwarnings("ignore")
@@ -7,44 +8,35 @@ from datasets.load_data import load_data
 from neural_network.neural_network import NeuralNetwork
 from neural_network.evolution_strategies import Solution
 
-from utils import split_data, tranform_data_onehot, create_layers
+from utils import get_split_indices, tranform_data_onehot, create_layers
 
 
 def main():
 
-    # neural network params
-    hidden_layer_size = 32
-    activation = 'leaky_relu'
-    n_hidden_layers = 1
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filename", required=True, type=str)
+    args = parser.parse_args()
 
-    # ES params
-    pop_size = 500
-    sigma = 0.01
-    learning_rate = 0.0001
-    epochs = 100
-    batch_size = 128
-    early_stopping_epochs = 20
+    with open(args.filename) as json_data:
+        data = json.load(json_data)  # data is a python dictionary
 
-    # objective function params
-    alpha_reg_pca = 0
-    partial_contribution_objective = True
-    num_components = 1
+    x = load_data(data["dataset"])
 
-    dataset = 'spheres'
-    x = load_data(dataset)
     x, num_features_per_network = tranform_data_onehot(x)  # transform categorical (object type in pandas) columns to one-hot encoded.
-    train_x, val_x = split_data(x)
+    train_indices, val_indices = get_split_indices(x)
+    train_x, val_x = x[train_indices], x[val_indices]
 
     # Instantiate Solution object
-    list_layers = [create_layers(n_features, n_hidden_layers, hidden_layer_size, activation) for n_features in num_features_per_network]
+    list_layers = [create_layers(n_features, data["n_hidden_layers"], data["hidden_layer_size"], data["activation"]) for n_features in num_features_per_network]
     list_neural_networks = [NeuralNetwork(l) for l in list_layers]
     solution = Solution(list_neural_networks)
 
     print("Training Baseline")
-    obj_list, x_transformed = solution.fit(train_x, val_x, sigma, learning_rate, pop_size, alpha_reg_pca, partial_contribution_objective, num_components,
-                                           epochs, batch_size, early_stopping_epochs)
+    obj_list, x_transformed = solution.fit(train_x, val_x, data["sigma"], data["learning_rate"], data["pop_size"], data["alpha_reg_pca"],
+                                           data["partial_contribution_objective"], data["num_components"], data["epochs"], data["batch_size"],
+                                           data["early_stopping_epochs"])
 
-    pickle.dump(x_transformed, open("../../Documents/x_transformed.p", "wb"))
+    pickle.dump((x_transformed, y), open("../../Documents/x_transformed.p", "wb"))
 
 
 if __name__ == "__main__":
