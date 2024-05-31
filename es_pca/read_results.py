e46387cdc1e50a70701b4532ddae6c8a1616f654
Thomas Uriot <uriot.thomas@gmail.com>
2024-05-31 11:15:35 +0200

    add function to plotting

--- es_pca/read_results.py
+++ es_pca/read_results.py
@@ -34,7 +34,22 @@ def compute_quantiles(fitness_list: np.array) -> tuple[np.array, np.array, np.ar
     return percentiles
 
 
-def plot_quantiles(results_dictionary, dataset_name: str = None) -> None:
+def retrieve_data(results_dictionary: dict, key: str) -> tuple[np.array, np.array]:
+
+    objective_list = [results_dictionary[key][index][0] for index in range(CONFIG["number_of_runs"])]
+    objective_val = np.reshape(np.array([tup[1] for element in objective_list for tup in element]),
+                               (CONFIG["number_of_runs"], CONFIG["epochs"]))
+    objective_train = np.reshape(np.array([tup[0] for element in objective_list for tup in element]),
+                                 (CONFIG["number_of_runs"], CONFIG["epochs"]))
+
+    return objective_val, objective_train
+
+
+def plot_quantiles(results_dictionary: dict, dataset_name: str = None) -> None:
+
+    # CB_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a',
+    #                   '#f781bf', '#a65628', '#984ea3',
+    #                   '#999999', '#e41a1c', '#dede00']
 
     color_list = ["darkblue", "darkgreen", "darkred", "darkorange"]
     legend_list = ["h=cos, objective=full",
@@ -44,32 +59,27 @@ def plot_quantiles(results_dictionary, dataset_name: str = None) -> None:
     legend_entries = []
     plt.figure(figsize=(10, 10))
     plt.ylim(1, 2.1)
-    plt.xlim(1, 100)
+    plt.xlim(1, CONFIG["epochs"])
 
     enumerate_counter = 0
     for key, value in results_dictionary.items():
-        objective_list = [results_dictionary[key][index][0] for index in range(CONFIG["number_of_runs"])]
-        objective_val = np.reshape(np.array([tup[1] for element in objective_list for tup in element]),
-                                   (CONFIG["number_of_runs"], CONFIG["epochs"]))
-        objective_train = np.reshape(np.array([tup[0] for element in objective_list for tup in element]),
-                                     (CONFIG["number_of_runs"], CONFIG["epochs"]))
+        objective_val, objective_train = retrieve_data(results_dictionary, key)
 
         percentiles_val = compute_quantiles(objective_val)
         percentiles_train = compute_quantiles(objective_train)
 
-        #####################################################################
         color = color_list[enumerate_counter]
 
         plt.plot(np.arange(1, CONFIG["epochs"] + 1, 1), percentiles_val[1], color=color)
         plt.plot(np.arange(1, CONFIG["epochs"] + 1, 1), percentiles_train[1], color=color, linestyle='--')
         legend_entries.append(legend_list[enumerate_counter])
         legend_entries.append(f"_")
-        plt.fill_between(np.arange(1, CONFIG["epochs"] + 1, 1), percentiles_val[0] + 0.001, percentiles_val[2] - 0.001,
-                         color=color, alpha=0.2)
-        plt.fill_between(np.arange(1, CONFIG["epochs"] + 1, 1), percentiles_train[0] + 0.001,
-                         percentiles_train[2] - 0.001, color=color, alpha=0.2)
-        legend_entries.append(f"_")
-        legend_entries.append(f"_")
+        # plt.fill_between(np.arange(1, CONFIG["epochs"] + 1, 1), percentiles_val[0] + 0.001, percentiles_val[2] - 0.001,
+        #                  color=color, alpha=0.2)
+        # plt.fill_between(np.arange(1, CONFIG["epochs"] + 1, 1), percentiles_train[0] + 0.001,
+        #                  percentiles_train[2] - 0.001, color=color, alpha=0.2)
+        # legend_entries.append(f"_")
+        # legend_entries.append(f"_")
 
         enumerate_counter += 1
 
