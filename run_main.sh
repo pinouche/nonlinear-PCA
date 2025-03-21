9163a8f775bb2254ef6d88bc1b68b4e9fd869213
pinouche <uriot.thomas@gmail.com>
2025-03-21 17:11:43 +0100

    add dependency

--- run_main.sh
+++ run_main.sh
@@ -1,6 +1,6 @@
 # Define the arrays for datasets, partial_contribution_objective values, activation values, and batch normalization values
 datasets=("german_credit")
-partial_contrib_values=("true")
+partial_contrib_values=("false")
 activation_values=("relu")
 
 # Loop over each combination of dataset, partial_contrib, activation, and batch_norm
