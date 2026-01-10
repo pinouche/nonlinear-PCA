c78b372d07097e889eac39fb5c3a3fea2ab671db
pinouche <uriot.thomas@gmail.com>
2026-01-10 22:52:18 +0100

    update notebooks

--- run_main.sh
+++ run_main.sh
@@ -1,5 +1,5 @@
 # Define the arrays for datasets, partial_contribution_objective values, activation values, and batch normalization values
-datasets=("wine")
+datasets=("spheres")
 partial_contrib_values=("true" "false")
 activation_values=("relu")
 
